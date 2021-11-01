cd06f338bef8bab79fe6c0d8e8eba5d5982b0995
Ronny Baturov <ronny1998@gmail.com>
2021-11-01 20:48:16 +0200

    Update BacktrackingBTree.java

--- src/BacktrackingBTree.java
+++ src/BacktrackingBTree.java
@@ -61,20 +61,19 @@ public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
 
 	private void merge(Node<T> splittedNode,Node<T> left,Node<T> right) {
 
-
-				if(splittedNode.parent == root && root.numOfKeys == 1) {
-					splittedNode.parent.removeChild(right); //Delete right child
-					splittedNode.parent.removeChild(left); //Delete right child
-					splittedNode.parent.removeKey(splittedNode.keys[splittedNode.getNumberOfKeys()/2]);//Remove the median key
-					root = splittedNode;}//Insert the node before the split has occurred.
-				else {
-					splittedNode.parent.removeChild(right); //Delete right child
-					splittedNode.parent.removeChild(left); //Delete right child
-					splittedNode.parent.removeKey(splittedNode.keys[splittedNode.getNumberOfKeys()/2]); //Remove the median key
-					splittedNode.parent.addChild(splittedNode);//Insert the node before the split has occurred.
+		if(splittedNode.parent == root && root.numOfKeys == 1) {
+			splittedNode.parent.removeChild(right); //Delete right child
+			splittedNode.parent.removeChild(left); //Delete right child
+			splittedNode.parent.removeKey(splittedNode.keys[splittedNode.getNumberOfKeys()/2]);//Remove the median key
+			root = splittedNode;
+		}//Insert the node before the split has occurred.
+		else {
+			splittedNode.parent.removeChild(right); //Delete right child
+			splittedNode.parent.removeChild(left); //Delete right child
+			splittedNode.parent.removeKey(splittedNode.keys[splittedNode.getNumberOfKeys()/2]); //Remove the median key
+			splittedNode.parent.addChild(splittedNode);//Insert the node before the split has occurred.
 				
-				}
-
+			}
 	}
 
 
@@ -89,128 +88,6 @@ public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
 //		System.out.println(tree.toString());;
 //		System.out.println("/////////////////////////////");
 //		tree.insert(5);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(2);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(122);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(17);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(23);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(80);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(70);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(60);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(54);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(32);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(95);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(25);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(7);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(1);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		tree.insert(0);
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-
-		for(int i = 0; i< 100; i++){
-			Random rand = new Random();
-			int rand_int1 = rand.nextInt(1000);
-			tree.insert(rand_int1);
-			System.out.println(tree.toString());;
-			System.out.println("/////////////////////////////");
-		}
-		for(int i = 0; i< 100; i++){
-			System.out.println("BackTrack" +i);
-			tree.Backtrack();
-			System.out.println(tree.toString());;
-			System.out.println("/////////////////////////////");
-		}
-//		System.out.println("BackTrack #1");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #2");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #3");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #4");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #5");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #6");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #7");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #8");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #9");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #10");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #11");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #12");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #13");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #14");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #15");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-//		System.out.println("/////////////////////////////");
-//		System.out.println("BackTrack #16");
-//		tree.Backtrack();
-//		System.out.println(tree.toString());;
-
 
 
 	}
